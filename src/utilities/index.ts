2d131e5c4a766befff4e4754e2bb99eedb2ec706
felix1251 <domhack123@gmail.com>
2023-03-04 09:48:00 +0800

    assign type for scrollIntoView method in utilities

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -1,4 +1,4 @@
 export const scrollIntoView = (id: string) => {
-    let location = document.getElementById(id);
-    if (location) location.scrollIntoView();
+    let location: HTMLElement | null = document.getElementById(id);
+    if (location) location.scrollIntoView() as void;
 };
